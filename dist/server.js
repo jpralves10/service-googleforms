a0426879a85bf6f6a1580917e15d0214b16a3f5a
Jean Alves <jpralves10@gmail.com>
2019-06-02 01:09:05 -0300

    Alteracoes

--- dist/server.js
+++ dist/server.js
@@ -75,8 +75,8 @@ server.use(function (err, req, res, next) { res.status(500).json(err); });
 //-----------------------------------------
 // Server Start
 //-----------------------------------------
-server.listen(process.env.PORT || 3443, function () {
-    console.log('Server is running on http://localhost:3443 || ' + process.env.PORT);
+server.listen(process.env.PORT || 8080, function () {
+    console.log('Server is running on http://localhost:8080 || ' + process.env.PORT);
 })
     .on('error', function (err) { return console.log(err); });
 //-----------------------------------------
