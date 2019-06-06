0f0163c63a0c331b65f5837d75ba14189dda7890
Jean Alves <jean@eficilog.com>
2019-06-06 16:03:26 -0300

    Alteracoes

--- src/config/privatekey.ts
+++ src/config/privatekey.ts
@@ -9,4 +9,4 @@ export default {
     "token_uri": "https://oauth2.googleapis.com/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
     "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/google-forms%40my-project-1498746435911.iam.gserviceaccount.com"
-}  
+}
