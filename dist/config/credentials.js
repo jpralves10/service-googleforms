a6670a1e36ccacac434431604d0cc2eae200c40c
Jean Alves <jpralves10@gmail.com>
2019-08-15 14:51:10 -0300

    Alteracoes

--- dist/config/credentials.js
+++ dist/config/credentials.js
@@ -1,11 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.MONGODB_HOST = process.env.MONGODB_HOST || '35.247.221.210:27017';
-exports.MONGODB_USER = process.env.MONGODB_USER || 'maestro';
-exports.MONGODB_PASSWORD = process.env.MONGODB_PASSWORD || 'eficimaestro';
-exports.MONGODB_AUTHDB = process.env.MONGODB_AUTHDB || 'crawler';
+exports.MONGODB_HOST = process.env.MONGODB_HOST || '35.198.13.166:27017';
+exports.MONGODB_USER = process.env.MONGODB_USER || 'catalogo';
+exports.MONGODB_PASSWORD = process.env.MONGODB_PASSWORD || 'QUsMspeS3Acx3wXm';
+exports.MONGODB_AUTHDB = process.env.MONGODB_AUTHDB || 'catalogo-produtos';
 //https://www.youtube.com/watch?v=cRbf_33Rdo4
 var URI = "mongodb://" + exports.MONGODB_USER + ":" + exports.MONGODB_PASSWORD + "@" + exports.MONGODB_HOST + "/" + exports.MONGODB_AUTHDB;
-var URI_LOCAL = "mongodb://@localhost:27017/crawler";
+//const URI_LOCAL = `mongodb://@localhost:27017/crawler`;
 //export const MONGODB_CONNECTIONSTRING = process.env.MONGODB_URI || URI
-exports.MONGODB_CONNECTIONSTRING = URI_LOCAL;
+exports.MONGODB_CONNECTIONSTRING = URI;
--- src/config/credentials.ts
+++ src/config/credentials.ts
@@ -1,12 +1,12 @@
-export const MONGODB_HOST = process.env.MONGODB_HOST || '35.247.221.210:27017';
-export const MONGODB_USER = process.env.MONGODB_USER || 'maestro';
-export const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD || 'eficimaestro';
-export const MONGODB_AUTHDB = process.env.MONGODB_AUTHDB || 'crawler';
+export const MONGODB_HOST = process.env.MONGODB_HOST || '35.198.13.166:27017';
+export const MONGODB_USER = process.env.MONGODB_USER || 'catalogo';
+export const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD || 'QUsMspeS3Acx3wXm';
+export const MONGODB_AUTHDB = process.env.MONGODB_AUTHDB || 'catalogo-produtos';
 
 //https://www.youtube.com/watch?v=cRbf_33Rdo4
 
 const URI = `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_HOST}/${MONGODB_AUTHDB}`;
-const URI_LOCAL = `mongodb://@localhost:27017/crawler`;
+//const URI_LOCAL = `mongodb://@localhost:27017/crawler`;
 
 //export const MONGODB_CONNECTIONSTRING = process.env.MONGODB_URI || URI
-export const MONGODB_CONNECTIONSTRING = URI_LOCAL
+export const MONGODB_CONNECTIONSTRING = URI
